1dfcb001b3ce8b32d9d256abf1bad10912d294a6
sisi <simdorvil@hotmail.com>
2016-07-05 17:59:07 +0000

    exercise 2

--- answers.js
+++ answers.js
@@ -5,6 +5,11 @@ function firstCharacter(string) {
 }
 
 //console.log(firstCharacter(""))
+function lastCharacter(string) {
+    return string.substring(string.length - 1, string.length);
+}
+
+//console.log(lastCharacter(""))
 
 
 
