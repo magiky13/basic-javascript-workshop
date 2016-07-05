27871aed8f367189f9634891be853e7a4afe3f2b
sisi <simdorvil@hotmail.com>
2016-07-05 17:48:56 +0000

    exercise one this just realized i can see what im typing here lol

---
+++ answers.js
@@ -0,0 +1,10 @@
+// this is a comment!
+//this function should take the first character of a string 
+function firstCharacter(string) {
+    return string.substring(0,1);
+}
+
+//console.log(firstCharacter(""))
+
+
+
